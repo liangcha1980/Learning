523ec6b7f6972d6a0f154423bcbfa73e60e122bf
liangcha1980 <67766517+liangcha1980@users.noreply.github.com>
2021-07-13 19:33:58 +0800

    Update pack_byte.sv

--- system verilog/pack_byte.sv
+++ system verilog/pack_byte.sv
@@ -1,4 +1,4 @@
-bit [15:0] l = 16'habed, m = 16'hcafe;
+bit [15:0] l = 16'habcd, m = 16'hcafe;
 bit [31:0] lm_pack ;
 lm_pack = {<< byte { l , m}};  // pack byte-size chunks
 $display ("%h" , lm_pack);    // displays “fecacdab”
